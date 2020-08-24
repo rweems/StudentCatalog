939493a7fb0d0664e153d8284a68c4ed09ceb99a
Rickey <weems505@gmail.com>
2020-08-24 11:05:06 -0500

    first commit

---
+++ src/test/java/com/example/studentcatalog/StudentCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studentcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
