a0a5bfccf42b3849a2c5c19126b23b445d4ac0ef
Rickey <weems505@gmail.com>
2020-08-25 16:12:23 -0500

    EOD

--- student-catalog/src/App.js
+++ student-catalog/src/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import Nav from 'react-bootstrap/Nav'
 
 import './App.css';
 // import Route from "react-router/modules/Route";
@@ -7,20 +8,38 @@ import AddStudent from "./components/AddStudent";
 import ViewStudents from "./components/ViewStudents";
 import {BrowserRouter} from "react-router-dom";
 import ViewProfStudents from "./components/ViewProfStudents";
+import Navbar from "react-bootstrap/Navbar";
+import Icon from "@material-ui/core/Icon";
 
 class App extends Component {
     render() {
         return (
+            <div>
+            {/*<Navbar>*/}
+            {/*    <Navbar.Header>*/}
+            {/*        <a href="#" className="navbar-brand logo">RSUITE</a>*/}
+            {/*    </Navbar.Header>*/}
+            {/*    <Navbar.Body>*/}
+            {/*        <Nav>*/}
+            {/*            <Nav.Item icon={<Icon icon="home" />} >Home</Nav.Item>*/}
+            {/*            <Nav.Item>News</Nav.Item>*/}
+            {/*            <Nav.Item>Products</Nav.Item>*/}
+            {/*        </Nav>*/}
+            {/*        <Nav pullRight>*/}
+            {/*            <Nav.Item icon={<Icon icon="cog" />} >Settings</Nav.Item>*/}
+            {/*        </Nav>*/}
+            {/*    </Navbar.Body>*/}
+            {/*</Navbar>*/}
             <Router>
-                <div>
+
                     <Switch>
                         <Route path="/students/show" component={ViewStudents}/>
                         <Route path="/professor/show" component={ViewProfStudents}/>
                         <Route path="/add" component={AddStudent} />
                     </Switch>
-                </div>
-            </Router>
 
+            </Router>
+            </div>
         );
     }
 }
